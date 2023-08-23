d877205bee2a134b48e897b4fdb749365d638054
Alexander <yearshow@yandex.ru>
2023-08-23 23:47:49 +0300

    Global styles setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
