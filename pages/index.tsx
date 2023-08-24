08bfa8ef707b107148e9362cc5ff9431fb34c1f8
yearshow <yearshow@yandex.ru>
2023-08-24 10:21:59 +0300

    File structure fix

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import {Button, Htag} from "../components";
+
+export default function Home() {
+  return (
+    <div>
+     <Htag tag="h1">Hello</Htag>
+        <Button appearance="primary">Hello World</Button>
+        <Button appearance="ghost">Hello World</Button>
+    </div>
+  )
+}
